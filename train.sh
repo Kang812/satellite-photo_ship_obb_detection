b6053d9241a24b538bb6080c9f795bf14e0b6461
Kang Won Byun <69283127+Kang812@users.noreply.github.com>
2024-10-13 22:35:45 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,15 +1,3 @@
-#best
-#python train.py --model_name yolo11l-obb \
-#    --dataset_yaml /workspace/oriented_dota/DOTA2.yaml \
-#    --epochs 100 \
-#    --batch 8 \
-#    --imgsz 640 \
-#    --workers 4 \
-#    --close_mosaic 10 \
-#    --device 0,1 \
-#    --seed 0 \
-#    --single_cls
-
 python train.py --model_name yolo11l-obb \
     --dataset_yaml /workspace/oriented_dota/DOTA3.yaml \
     --epochs 100 \
